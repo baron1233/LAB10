19bbffc9954480f3ba6c67328b876f01728b6277
unknown <thanapon.tha@ku.th>
2026-09-16 17:09:34 +0700

    First commit: Add JS logo and footer

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -10,20 +10,20 @@ function Portfolio() {
         <div className={styles.port_items}>
           <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
             <img
-              src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              src="9181370.jpg"
               alt="coding"
             />
           </Tilt>
-          <p>Project Title</p>
+          <p>DEV</p>
         </div>
         <div className={styles.port_items}>
           <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
             <img
-              src="https://images.unsplash.com/photo-1555066931-4365d14bab8c?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              src="2716.jpg"
               alt="coding"
             />
           </Tilt>
-          <p>Project Title</p>
+          <p>DEV X</p>
         </div>
         <div className={styles.port_items}>
           <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
